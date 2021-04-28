1c48ba9cf8b2cd153373a66bfd05944923ea1fd9
Anan Wolf <ananwolf1@gmail.com>
2021-04-28 16:09:30 -0700

    basic functional approach complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,28 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var size = 0;
+  var frontQueue = 0;
+  var backQueue = 0;
   someInstance.enqueue = function(value) {
+    backQueue++;
+    storage[backQueue] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    if (size === 0) {
+      return;
+    }
+    frontQueue++;
+    var keyToBeDeleted = storage[frontQueue];
+    delete storage[frontQueue];
+    size--;
+    return keyToBeDeleted;
   };
 
   someInstance.size = function() {
+    return size < 0 ? 0 : size;
   };
 
   return someInstance;
