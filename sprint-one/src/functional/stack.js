68707867588950d6091cbb5a98929ededafad17a
Anan Wolf <ananwolf1@gmail.com>
2021-04-28 12:30:06 -0700

    First Stack finished up until "allows sequentially adding and removing items" test

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,14 +5,27 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
+  var size = 0;
+  var currentKey = 1;
   someInstance.push = function(value) {
+    storage[currentKey] = value;
+    currentKey++;
+    size++;
   };
 
   someInstance.pop = function() {
+    delete storage.currentKey;
+    currentKey--;
+    size--;
   };
 
   someInstance.size = function() {
+    return size < 0 ? 0 : size;
   };
 
   return someInstance;
 };
+
+// ReferenceError: stroage is not defined
+//     at Object.Stack.someInstance.size (src/functional/stack.js:23:7)
+//     at Context.<anonymous> (spec/specs.js:44:22)
