08056b24086ece960a62b79fe6d5563c9bc1875e
Anan Wolf <ananwolf1@gmail.com>
2021-04-28 23:21:04 -0700

    part 2 complete using _.extend and this

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,34 @@
 var Queue = function() {
-
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  storage.frontQueue = 0;
+  storage.backQueue = 0;
+  storage.amount = 0;
+  _.extend(storage, queueMethods);
+  return storage;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.backQueue++;
+  this.amount++;
+  this[this.backQueue] = value;
+};
+
+queueMethods.dequeue = function() {
+  this.frontQueue++;
+  var dequeued = this[this.frontQueue];
+  delete this[this.frontQueue];
+  this.amount--;
+  if (this.amount < 0) {
+    this.amount++;
+  }
+  return dequeued;
+};
+
+queueMethods.size = function() {
+  return this.amount;
+};
 
