92eb751c0d805b6518c743a4c06f4ed88527a143
Anan Wolf <ananwolf1@gmail.com>
2021-04-29 10:24:13 -0700

    Psuedoclass completed

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.frontQueue = 0;
+  this.backQueue = 0;
+  this.amount = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.backQueue++;
+  this.amount++;
+  this[this.backQueue] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  this.frontQueue++;
+  this.amount--;
+  if (this.amount < 0) {
+    this.amount++;
+  }
+  var dequeued = this[this.frontQueue];
+  delete this[this.frontQueue];
+  return dequeued;
+};
+
+Queue.prototype.size = function() {
+  return this.amount;
+};
+
+var storage = new Queue();
