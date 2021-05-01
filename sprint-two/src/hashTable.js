22ee22712b89b91be582781c14424a5d6a0fc5c7
Anan Wolf <ananwolf1@gmail.com>
2021-04-30 20:21:15 -0700

    working on HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,10 +7,17 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index] === undefined) {
+    this._storage[index] = v;
+  } else {
+
+  }
+  // console.log(this._storage)
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage[index];
 };
 
 HashTable.prototype.remove = function(k) {
